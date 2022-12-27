58349f49648483ee48717ec168630997db559b5b
daramjiyun <315ljy@gmail.com>
2022-12-27 22:34:09 +0900

    navbar 구현중

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -3,18 +3,69 @@
     <v-app-bar color="indigo lighten-5" elevation="5">
       KYOL X WYL
       <v-spacer></v-spacer>
-      <v-avatar
-        color=" indigo lighten-2"
-        size="56"
-        margin-left="50px"
-      ></v-avatar>
     </v-app-bar>
+    <template>
+      <div v-if="!loginCheck">
+        <v-container fluid style="height: 300px">
+          <v-row justify="center">
+            <v-menu bottom min-width="200px" rounded offset-y>
+              <template v-slot:activator="{ on }">
+                <v-btn class="d" icon x-large v-on="on">
+                  <v-avatar color="brown" size="48">
+                    <span class="white--text text-h7"> NO </span>
+                  </v-avatar>
+                </v-btn>
+              </template>
+            </v-menu>
+          </v-row>
+        </v-container>
+      </div>
+      <div v-if="loginCheck">
+        <v-container fluid style="height: 300px">
+          <v-row justify="center">
+            <v-menu bottom min-width="200px" rounded offset-y>
+              <template v-slot:activator="{ on }">
+                <v-btn class="d" icon x-large v-on="on">
+                  <v-avatar color="brown" size="48">
+                    <span class="white--text text-h5"> YES </span>
+                  </v-avatar>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-list-item-content class="justify-center">
+                  <div class="mx-auto text-center">
+                    <v-avatar color="brown">
+                      <span class="d white--text text-h5">user.initials</span>
+                    </v-avatar>
+                    <h3>user.fullName</h3>
+                    <p class="text-caption mt-1">user.email</p>
+                    <v-divider class="my-3"></v-divider>
+                    <v-btn depressed rounded text> Edit Account </v-btn>
+                    <v-divider class="my-3"></v-divider>
+                    <v-btn depressed rounded text> Disconnect </v-btn>
+                  </div>
+                </v-list-item-content>
+              </v-card>
+            </v-menu>
+          </v-row>
+        </v-container>
+      </div>
+    </template>
   </div>
 </template>
+
 
 <script>
 export default {
   name: 'NavBar',
+  computed: {
+    loginCheck() {
+      return this.$store.getters['TokenUser']
+    },
+    TokenUserName() {
+      return this.$store.getters.TokenUser && this.$store.getters.TokenUser.UserId
+    }
+  },
 }
 </script>
 
@@ -26,5 +77,17 @@ export default {
 font-family: 'Hahmlet', serif;
 font-weight: bold;
 font-size: 25px;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  flex: 1 1 auto;
+  margin: -67px;
+  }
+.d{
+  /* align-items: center; */
+  margin-right: -1410px;
+  margin-top: -3px;
 }
 </style>
